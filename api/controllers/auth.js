2eab0655115098e0ac6c8af6472462edd0ec0be0
Cajaye Clarke <cajayeclarke44@gmail.com>
2022-01-05 15:10:39 -0500

    Set up login route and check for valid paswords and email address

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,5 +1,12 @@
 const { StatusCodes } = require('http-status-codes')
+const { UnauthorizedError, BadRequest } = require('../errors/index')
 const User = require('../models/User')
+const bcrypt = require('bcryptjs')
+
+const matchPassword = async (enteredpassword, oldPassword) => {
+    return await bcrypt.compare(enteredpassword, oldPassword)
+}
+
 const register = async (req, res) => {
     const user = await User.create({ ...req.body })
     const token = user.createJWT()
@@ -7,6 +14,17 @@ const register = async (req, res) => {
 }
 
 const login = async (req, res) => {
+    const { email, password } = req.body;
+    if (!email || !password) {
+        throw new BadRequest('Please provide both email and password')
+    }
+    const user = await User.findOne({ email: email })
+    if (!user) throw new UnauthorizedError('Email address does not exist try signing up')
+
+    const comparePassword = await matchPassword(password, user.password)
+    if (!comparePassword) throw new UnauthorizedError('Invalid password')
+
+    res.status(StatusCodes.OK).json('Token')
 
 }
 
