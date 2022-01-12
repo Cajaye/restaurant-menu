74e2df9578ee9b9c105c634e4dd74722c56f0651
Cajaye Clarke <cajayeclarke44@gmail.com>
2022-01-12 13:56:48 -0500

    Made loader a compenent so I can reuse it

--- src/components/Cart.svelte
+++ src/components/Cart.svelte
@@ -2,6 +2,7 @@
   import Button from "./Button.svelte";
   import OperationButton from "./OperationButton.svelte";
   import { token } from "../stores/token";
+  import Loader from "./Loader.svelte";
 
   const BearerToken = `Bearer ${$token}`;
   const url: string = "http://localhost:5000/api/v1/cart";
@@ -96,27 +97,6 @@
       </div>
     </div>
   {:else}
-    <div class="mt-40 ml-80">
-      <svg
-        class="animate-spin h-16 w-16 text-JetBlack"
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-      >
-        <circle
-          class="opacity-25"
-          cx="12"
-          cy="12"
-          r="10"
-          stroke="currentColor"
-          stroke-width="4"
-        />
-        <path
-          class="opacity-75"
-          fill="currentColor"
-          d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-        />
-      </svg>
-    </div>
+    <Loader />
   {/each}
 </section>
