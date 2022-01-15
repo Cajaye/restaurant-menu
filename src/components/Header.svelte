cdbc23cbd9fd0995ccb1837084d8dd746f54fb3d
Cajaye Clarke <cajayeclarke44@gmail.com>
2022-01-14 22:46:01 -0500

    logout functionality added

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
-  import { Link } from "svelte-navigator";
+  import { Link, useNavigate } from "svelte-navigator";
   import { itemsInCart } from "../stores/cartstore";
   import { token } from "../stores/token";
+  const navigate = useNavigate();
   $: isAuthenticated = $token;
   let toggleMenu = false;
   const toggle = (bool: boolean): boolean => {
@@ -57,35 +58,33 @@
         <li class="navItems">
           <a href="gallery.com">Gallery</a>
         </li>
-        {#if isAuthenticated}
-          <Link to="cart">
-            <li class="navItems relative mt-5 md:mt-0">
-              <div
-                class="bg-JetBlack p-0.5 rounded-full w-5 h-5 text-xs absolute top-[-14px] md:right-[-17px] left-5"
-              >
-                <p class="text-center text-white font-bold">
-                  {$itemsInCart}
-                </p>
-              </div>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                class="h-6 w-6 inline cursor-pointer mb-1.5 md:mb-0"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
-                />
-              </svg>
-            </li>
-          </Link>
-        {:else}
-          <Link to="login" />
-        {/if}
+
+        <Link to="cart">
+          <li class="navItems relative mt-5 md:mt-0">
+            <div
+              class="bg-JetBlack p-0.5 rounded-full w-5 h-5 text-xs absolute top-[-14px] md:right-[-17px] left-5"
+            >
+              <p class="text-center text-white font-bold">
+                {$itemsInCart}
+              </p>
+            </div>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-6 w-6 inline cursor-pointer mb-1.5 md:mb-0"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+              />
+            </svg>
+          </li>
+        </Link>
+
         <!--If token does not exist show the login and resiter buttons if it does exists show to logout button-->
         {#if $token === null}
           <li class="inline mr-2">
@@ -111,6 +110,13 @@
         {:else}
           <li class="md:inline mr-2 text-center my-2 md:my-2">
             <input
+              on:click={() => {
+                localStorage.removeItem("token");
+                window.location.reload();
+                if (!$token) {
+                  navigate("/");
+                }
+              }}
               class="rounded-md px-2 py-px bg-JetBlack text-white cursor-pointer font-semibold"
               type="button"
               value="logout"
